72e26e97aac8744ecefbc08673d03c33a9af3382
Rutger Kool <rutgerkool@gmail.com>
2025-03-30 08:33:07 +0200

    Refactor TeamListModel class.

--- include/gui/models/TeamListModel.h
+++ include/gui/models/TeamListModel.h
@@ -3,29 +3,34 @@
 
 #include "models/PlayerRating.h"
 #include "services/TeamManager.h"
-#include <QtCore/QAbstractTableModel>
 #include <QtCore/QAbstractListModel>
 #include <QtCore/QString>
 #include <vector>
+#include <span>
 
-class TeamListModel : public QAbstractListModel {
+class TeamListModel final : public QAbstractListModel {
     Q_OBJECT
 
     public:
-        explicit TeamListModel(TeamManager& teamManager, QObject *parent = nullptr);
+        explicit TeamListModel(TeamManager& teamManager, QObject* parent = nullptr);
+        ~TeamListModel() override = default;
 
-        int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-        
-        Qt::ItemFlags flags(const QModelIndex &index) const override;
-        bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+        TeamListModel(const TeamListModel&) = delete;
+        TeamListModel& operator=(const TeamListModel&) = delete;
+        TeamListModel(TeamListModel&&) = delete;
+        TeamListModel& operator=(TeamListModel&&) = delete;
+
+        [[nodiscard]] int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+        [[nodiscard]] QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+        [[nodiscard]] Qt::ItemFlags flags(const QModelIndex& index) const override;
+        bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole) override;
 
     public slots:
         void refresh();
 
     private:
-        TeamManager& teamManager;
-        std::vector<Team> teams;
+        TeamManager& m_teamManager;
+        std::vector<Team> m_teams;
 };
 
 #endif
--- src/gui/models/TeamListModel.cpp
+++ src/gui/models/TeamListModel.cpp
@@ -1,69 +1,77 @@
 #include "gui/models/TeamListModel.h"
 #include <algorithm>
+#include <ranges>
 
-TeamListModel::TeamListModel(TeamManager& tm, QObject *parent)
+TeamListModel::TeamListModel(TeamManager& teamManager, QObject* parent)
     : QAbstractListModel(parent)
-    , teamManager(tm)
+    , m_teamManager(teamManager)
 {
     refresh();
 }
 
-int TeamListModel::rowCount(const QModelIndex &parent) const {
-    if (parent.isValid()) return 0;
-    return static_cast<int>(teams.size());
+int TeamListModel::rowCount(const QModelIndex& parent) const {
+    if (parent.isValid()) {
+        return 0;
+    }
+    
+    return static_cast<int>(m_teams.size());
 }
 
-QVariant TeamListModel::data(const QModelIndex &index, int role) const {
-    if (!index.isValid()) return QVariant();
-
-    if (role == Qt::DisplayRole) {
-        return QString::fromStdString(teams[index.row()].teamName);
-    }
-    else if (role == Qt::UserRole) {
-        return teams[index.row()].teamId;
+QVariant TeamListModel::data(const QModelIndex& index, int role) const {
+    if (!index.isValid() || index.row() >= static_cast<int>(m_teams.size())) {
+        return {};
     }
 
-    return QVariant();
+    const auto& team = m_teams[index.row()];
+    
+    switch (role) {
+        case Qt::DisplayRole:
+            return QString::fromStdString(team.teamName);
+        case Qt::UserRole:
+            return team.teamId;
+        default:
+            return {};
+    }
 }
 
-Qt::ItemFlags TeamListModel::flags(const QModelIndex &index) const {
-    if (!index.isValid()) return Qt::NoItemFlags;
+Qt::ItemFlags TeamListModel::flags(const QModelIndex& index) const {
+    if (!index.isValid()) {
+        return Qt::NoItemFlags;
+    }
     
     return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
 }
 
-bool TeamListModel::setData(const QModelIndex &index, const QVariant &value, int role) {
-    if (!index.isValid() || static_cast<size_t>(index.row()) >= teams.size() || role != Qt::EditRole) {
+bool TeamListModel::setData(const QModelIndex& index, const QVariant& value, int role) {
+    if (!index.isValid() || 
+        index.row() >= static_cast<int>(m_teams.size()) || 
+        role != Qt::EditRole) {
         return false;
     }
     
-    int teamId = teams[index.row()].teamId;
-    QString newName = value.toString();
+    const QString newName = value.toString();
     
     if (newName.isEmpty()) {
         return false;
     }
     
-    bool success = teamManager.updateTeamName(teamId, newName.toStdString());
+    const int teamId = m_teams[index.row()].teamId;
     
-    if (success) {
-        teams[index.row()].teamName = newName.toStdString();
-        emit dataChanged(index, index, {Qt::DisplayRole});
+    if (!m_teamManager.updateTeamName(teamId, newName.toStdString())) {
+        return false;
     }
     
-    return success;
+    m_teams[index.row()].teamName = newName.toStdString();
+    emit dataChanged(index, index, {Qt::DisplayRole});
+    
+    return true;
 }
 
 void TeamListModel::refresh() {
     beginResetModel();
-    teams.clear();
     
-    auto allTeams = teamManager.getAllTeams();
-    teams.reserve(allTeams.size());
-    
-    for (auto& team : allTeams) {
-        teams.push_back(std::move(team));
-    }
+    auto allTeams = m_teamManager.getAllTeams();
+    m_teams = std::move(allTeams);
     
     endResetModel();
 }
